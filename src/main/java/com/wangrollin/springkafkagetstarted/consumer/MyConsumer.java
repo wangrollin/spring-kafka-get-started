cc606d8f5eec9b3164ec04243577b09798267a3c
wangrollin <wangrollin@foxmail.com>
2021-01-20 13:36:26 +0800

    use avro as data format

--- src/main/java/com/wangrollin/springkafkagetstarted/consumer/MyConsumer.java
+++ src/main/java/com/wangrollin/springkafkagetstarted/consumer/MyConsumer.java
@@ -1,14 +1,23 @@
 package com.wangrollin.springkafkagetstarted.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import com.wangrollin.avro.generated.User;
 
 @Service
 public class MyConsumer {
 
-    @KafkaListener(id = "fooGroup", topics = "users")
-    public void listen(String userMsg) {
+//    @KafkaListener(id = "fooGroup", topics = "users")
+//    public void listen(String userMsg) {
+//
+//        System.out.println("Received: " + userMsg);
+//    }
 
-        System.out.println("Received: " + userMsg);
+    @KafkaListener(id = "avroGroup", topics = "avroUsers")
+    public void listen(ConsumerRecord<String, User> record) {
+
+        System.out.println("Received: " + record.value().getName());
     }
 }
--- src/main/java/com/wangrollin/springkafkagetstarted/producer/MyProducer.java
+++ src/main/java/com/wangrollin/springkafkagetstarted/producer/MyProducer.java
@@ -1,5 +1,6 @@
 package com.wangrollin.springkafkagetstarted.producer;
 
+import com.wangrollin.avro.generated.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,20 @@ public class MyProducer {
     @Autowired
     private KafkaTemplate<Object, Object> template;
 
-    @PostConstruct
-    public void produceMsg() {
+//    @PostConstruct
+//    public void produceMsg() {
+//
+//        System.out.println("start");
+//        this.template.send("users", "second user");
+//        System.out.println("end");
+//    }
 
-        System.out.println("start");
-        this.template.send("users", "second user");
-        System.out.println("end");
-    }
+//    @PostConstruct
+//    public void produceMsg() {
+//
+//        System.out.println("start");
+//        User user = new User("name1", 1);
+//        this.template.send("avroUsers", user.getName(), user);
+//        System.out.println("end");
+//    }
 }
